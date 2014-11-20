f006e72c439d82c527d7158514aa99837e96d5d4
Hirokazu <saigyoji205@gmail.com>
2014-11-20 22:50:43 +0900

    Initial Commit

---
+++ Apple_Watch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Apple_Watch
+//
+//  Created by Hirokazu on 2014/11/20.
+//  Copyright (c) 2014年 Hirokazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
